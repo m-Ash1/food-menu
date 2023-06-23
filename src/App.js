93480a97f0f0d7aefc10104469a0c60d4b5ce68d
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-06-23 18:42:24 +0300

    github pages conf

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
 
 const Pizza = ({ pizzaObj }) => {
   return (
-    <li className={`pizza${pizzaObj.soldOut ? "sold-out" : ""}`}>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
